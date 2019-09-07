cfb10161f7f6f8444245d0a2d153b74bf5863b85
Ata Mahmoud <ata.ata250@hotmail.com>
2019-09-07 19:28:13 +0200

    plugin init

---
+++ android/src/main/java/com/scissorboy/flutter_zendesk/FlutterZendeskPlugin.java
@@ -0,0 +1,41 @@
+package com.scissorboy.flutter_zendesk;
+
+import android.content.Intent;
+
+import com.zopim.android.sdk.api.ZopimChat;
+import com.zopim.android.sdk.model.VisitorInfo;
+import com.zopim.android.sdk.prechat.ZopimChatActivity;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/**
+ * FlutterZendeskPlugin
+ */
+public class FlutterZendeskPlugin implements MethodCallHandler {
+    private final Registrar registrar;
+
+    private FlutterZendeskPlugin(Registrar registrar) {
+        this.registrar = registrar;
+    }
+
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_zendesk");
+        channel.setMethodCallHandler(new FlutterZendeskPlugin(registrar));
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+    }
+}
